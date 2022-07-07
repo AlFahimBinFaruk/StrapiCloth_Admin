ba46550ea1d781bb7bc39aa4680927c76c3b0bfb
al fahim bin faruk <fmd03700@gmail.com>
2022-07-07 13:43:52 +0600

    category,product collection added,cloudinary configured

--- config/server.js
+++ config/server.js
@@ -1,7 +1,18 @@
-module.exports = ({ env }) => ({
+const productionServer = ({ env }) => ({
   proxy: true,
-  url: env('MY_HEROKU_URL'),
-  app: { 
-    keys: env.array('APP_KEYS')
+  url: env("MY_HEROKU_URL"),
+  app: {
+    keys: env.array("APP_KEYS"),
   },
-})
+});
+
+const devServer = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
+
+module.exports =
+  process.env.NODE_ENV == "production" ? productionServer : devServer;
