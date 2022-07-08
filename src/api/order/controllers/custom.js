531baca8dc81bb954a6adc9058b7c703cb60872f
al fahim bin faruk <fmd03700@gmail.com>
2022-07-08 22:25:08 +0600

    order api updated

--- src/api/order/controllers/custom.js
+++
@@ -1,7 +0,0 @@
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::order.order', ({ strapi }) =>  ({
-    async findMyOrders(ctx, next) { // called by GET /hello 
-        ctx.body = 'Hello World!'; // we could also send a JSON
-      },
-}));
